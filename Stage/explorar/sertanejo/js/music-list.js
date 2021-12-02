dd3edad0fa8d26523f2c410cb0d0c8f55291344b
Henrique Sanches Silva <87503412+HenriqueS463@users.noreply.github.com>
2021-12-02 00:52:07 -0300

    Explorar

---
+++ Stage/explorar/sertanejo/js/music-list.js
@@ -0,0 +1,15 @@
+let allMusic = [
+    {
+      name: "Novo Drip",
+      artist: "Recayd",
+      img: "music-1",
+      src: "music-1",
+    },
+    {
+      name:"Honda",
+      artist:"Jovem Dex",
+      img: "music-2",
+      src:"music-2" 
+    }
+  ];
+  
